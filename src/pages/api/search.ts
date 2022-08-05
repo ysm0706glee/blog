71cab9ad7851594c98e02a5355a11370b0ac3c30
Takuma <ysm0706glee@gmail.com>
2022-08-05 22:24:03 +0200

    done

---
+++ src/pages/api/search.ts
@@ -0,0 +1,15 @@
+import type { NextApiRequest, NextApiResponse } from "next";
+import { client } from "src/libs/client";
+import { Blog } from "src/pages";
+
+const handler = async (req: NextApiRequest, res: NextApiResponse) => {
+  console.log(req.body.q);
+
+  const data = await client.getList<Blog>({
+    endpoint: "blog",
+    queries: { q: req.body.q },
+  });
+  res.status(200).json(data);
+};
+
+export default handler;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,8 @@
+import Link from "next/link";
 import { GetStaticProps, NextPage } from "next";
+import { ComponentProps, useState } from "react";
 import { MicroCMSListResponse } from "microcms-js-sdk";
 import { client } from "src/libs/client";
-import Link from "next/link";
 
 export type Blog = {
   title: string;
@@ -11,11 +12,43 @@ export type Blog = {
 type Props = MicroCMSListResponse<Blog>;
 
 const Home: NextPage<Props> = (props) => {
+  const [search, setSearch] = useState<MicroCMSListResponse<Blog>>();
+
+  const handleSubmit: ComponentProps<"form">["onSubmit"] = async (e) => {
+    e.preventDefault();
+    const q = e.currentTarget.query.value;
+    const data = await fetch("/api/search", {
+      method: "post",
+      headers: { "Content-type": "application/json" },
+      body: JSON.stringify({ q }),
+    });
+    const json: MicroCMSListResponse<Blog> = await data.json();
+    setSearch(json);
+  };
+
+  const handleClick: ComponentProps<"button">["onClick"] = () => {
+    setSearch(undefined);
+  };
+
+  const contents = search ? search.contents : props.contents;
+  const totalCount = search ? search.totalCount : props.totalCount;
+
   return (
     <div>
-      <p className="text-gray-400">{props.totalCount}</p>
+      <form className="flex gap-x-2" onSubmit={handleSubmit}>
+        <input type="text" name="query" className="border border-black px-2" />
+        <button className="border border-black px-2">Search</button>
+        <button
+          type="reset"
+          className="border border-black px-2"
+          onClick={handleClick}
+        >
+          Reset
+        </button>
+      </form>
+      <p className="text-gray-400">{search ? totalCount : props.totalCount}</p>
       <ul className="mt-4 space-y-4">
-        {props.contents.map((content) => (
+        {contents.map((content) => (
           <li key={content.id}>
             <Link href={`/blog/${content.id}`}>
               <a className="text-xl text-blue-800 underline hover:text-blue-400">
