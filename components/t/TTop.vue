cca2011789d995cfed7eec4b192831201d4c316a
Takuma Yoshimi <ysm0706glee@gmail.com>
2023-09-22 15:58:42 +0200

    add index page

---
+++ components/t/TTop.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+const { blogsState, getBlogs } = useBlog();
+
+try {
+  await getBlogs();
+} catch (error) {
+  console.error(error);
+}
+</script>
+
+<template>
+  <ul>
+    <li v-for="blog in blogsState" :key="blog.id">
+      <a :href="blog.url">
+        {{ blog.title }}
+        {{ blog.description }}
+        <template v-if="blog.picture">
+          <img :src="blog.picture" :alt="`${blog.title}'s picture'`" />
+        </template>
+      </a>
+    </li>
+  </ul>
+</template>
