c71a25443ec0e778555d2c1bf570ef78f3d72d02
Takuma Yoshimi <ysm0706glee@gmail.com>
2023-09-24 11:06:26 +0200

    add postBlog

--- composables/useBlog.ts
+++ composables/useBlog.ts
@@ -1,7 +1,8 @@
 import { Blog } from "~/types/blog";
+import { Database } from "~/types/supabase";
 
 const useBlog = () => {
-  const supabase = useSupabaseClient();
+  const supabase = useSupabaseClient<Database>();
 
   const blogsState = useState<Blog[]>("blogsState", () => []);
 
@@ -15,9 +16,33 @@ const useBlog = () => {
     return blogs;
   };
 
+  const postBlog = async (
+    blog: Pick<Blog, "url" | "title" | "description" | "image">
+  ): Promise<Blog | null> => {
+    const { data: newBlog, error } = await supabase
+      .from("blogs")
+      .insert({
+        url: blog.url,
+        title: blog.title,
+        description: blog.description,
+        image: blog.image,
+      })
+      .select();
+
+    console.log("newBlog: ", newBlog);
+
+    if (error) {
+      console.error(error);
+      return null;
+    }
+    blogsState.value = [newBlog[0], ...blogsState.value];
+    return newBlog[0];
+  };
+
   return {
     blogsState,
     getBlogs,
+    postBlog,
   };
 };
 
