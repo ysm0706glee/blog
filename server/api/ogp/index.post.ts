0b00d8b282cb11d3e7ce5b95e052d8a50ca87edb
Takuma Yoshimi <ysm0706glee@gmail.com>
2023-09-27 15:26:14 +0200

    refactor ogp api

--- server/api/ogp/index.post.ts
+++ server/api/ogp/index.post.ts
@@ -1,25 +1,29 @@
 import cheerio from "cheerio";
+import { Blog } from "~/types/blog";
 
-export default defineEventHandler(async (event) => {
-  try {
-    const { url } = await readBody(event);
-    const response = await fetch(url);
-    const html = await response.text();
-    const $ = cheerio.load(html);
-    const title = $("meta[property='og:title']").attr("content");
-    const image = $("meta[property='og:image']").attr("content");
-    const description = $("meta[property='og:description']").attr("content");
-    return {
-      title,
-      image,
-      description,
-    };
-  } catch (e) {
-    console.error(e);
-    return {
-      title: "",
-      image: "",
-      description: "",
-    };
+export default defineEventHandler(
+  async (event): Promise<Pick<Blog, "title" | "description" | "image">> => {
+    try {
+      const { url } = await readBody(event);
+      const response = await fetch(url);
+      const html = await response.text();
+      const $ = cheerio.load(html);
+      const title = $("meta[property='og:title']").attr("content") || "";
+      const image = $("meta[property='og:image']").attr("content") || "";
+      const description =
+        $("meta[property='og:description']").attr("content") || "";
+      return {
+        title,
+        image,
+        description,
+      };
+    } catch (e) {
+      console.error(e);
+      return {
+        title: "",
+        image: "",
+        description: "",
+      };
+    }
   }
-});
+);
