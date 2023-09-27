ba2013b607535acae21556013334b1f93241a697
Takuma Yoshimi <ysm0706glee@gmail.com>
2023-09-27 15:27:08 +0200

    style index page

--- components/t/TTop.vue
+++ components/t/TTop.vue
@@ -10,12 +10,25 @@ try {
 
 <template>
   <ul>
-    <li v-for="blog in blogsState" :key="blog.id">
-      <a :href="blog.url">
-        {{ blog.title }}
-        {{ blog.description }}
-        <template v-if="blog.picture">
-          <img :src="blog.picture" :alt="`${blog.title}'s picture'`" />
+    <li
+      v-for="blog in blogsState"
+      :key="blog.id"
+      class="p-4 border-b border-gray-700"
+    >
+      <a
+        :href="blog.url"
+        class="flex items-center space-x-4 hover:text-gray-400"
+      >
+        <div class="flex-1">
+          <h2 class="text-lg font-semibold">{{ blog.title }}</h2>
+          <p class="text-sm">{{ blog.description }}</p>
+        </div>
+        <template v-if="blog.image">
+          <img
+            :src="blog.image"
+            :alt="`${blog.title}'s picture`"
+            class="w-20 h-20 object-cover rounded"
+          />
         </template>
       </a>
     </li>
