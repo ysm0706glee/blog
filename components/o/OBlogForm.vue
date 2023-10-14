a0355d78741bffb16ad48c1db69e7cf8eb4d4a0c
Takuma Yoshimi <ysm0706glee@gmail.com>
2023-10-14 10:24:38 +0200

    add tags filter in admin page

--- components/o/OBlogForm.vue
+++ components/o/OBlogForm.vue
@@ -3,6 +3,8 @@ import { useField, useForm } from "vee-validate";
 import { toTypedSchema } from "@vee-validate/zod";
 import * as zod from "zod";
 import { Blog } from "~/types/blog";
+import { Tag } from "~/types/tag";
+import { tagInjectionKey } from "~/composables/useTag";
 
 type Props = {
   blogData: Pick<Blog, "url" | "title" | "description" | "image">;
@@ -11,6 +13,9 @@ type Props = {
 const props = defineProps<Props>();
 
 const { postBlog } = useBlog();
+
+// TODO: ! is not safe
+const { tagState, selectedTags, getTags } = inject(tagInjectionKey)!;
 
 const validationSchema = toTypedSchema(
   zod.object({
@@ -47,17 +52,45 @@ const fields = {
 
 const { title, description, image } = fields;
 
+const isOpenAddTagModal = ref(false);
+
 const onPost = handleSubmit(async ({ url, title, description, image }) => {
-  const response = await postBlog({
-    url,
-    title,
-    description,
-    image,
-  });
+  const response = await postBlog(
+    {
+      url,
+      title,
+      description,
+      image,
+    },
+    selectedTags.value
+  );
   response
     ? useNuxtApp().$toast.success("success")
     : useNuxtApp().$toast.error("error");
 });
+
+const toggleTag = (tagId: Tag["id"]) => {
+  const tag = tagState.value.find((tag) => tag.id === tagId);
+  if (!tag) return;
+  if (selectedTags.value.some((selectedTag) => selectedTag.id === tagId)) {
+    selectedTags.value = selectedTags.value.filter(
+      (selectedTag) => selectedTag.id !== tagId
+    );
+  } else {
+    selectedTags.value = [tag, ...selectedTags.value];
+  }
+};
+
+const openAddTagModal = () => {
+  isOpenAddTagModal.value = true;
+};
+
+try {
+  await getTags();
+} catch (error) {
+  console.error(error);
+  useNuxtApp().$toast.error("error");
+}
 </script>
 
 <template>
@@ -76,6 +109,31 @@ const onPost = handleSubmit(async ({ url, title, description, image }) => {
       <label for="image">Image</label>
       <UInput id="image" v-model="image.value.value" />
       <span>{{ errors.image }}</span>
+    </div>
+    <div>
+      <label for="image">Tags</label>
+      <template v-if="tagState.length">
+        <UBadge
+          v-for="tag in tagState"
+          :key="tag.id"
+          :label="tag.name"
+          :color="
+            selectedTags.some((selectedTag) => selectedTag.id === tag.id)
+              ? 'primary'
+              : 'gray'
+          "
+          class="cursor-pointer"
+          @click="toggleTag(tag.id)"
+        />
+      </template>
+      <div>
+        <UButton label="Open" @click="openAddTagModal" />
+        <OAddTagModal
+          :is-open-add-tag-modal="isOpenAddTagModal"
+          @update:is-open-add-tag-modal="isOpenAddTagModal = $event"
+          @toggle-tag="toggleTag"
+        />
+      </div>
     </div>
     <UButton @click="onPost">Post</UButton>
   </form>
