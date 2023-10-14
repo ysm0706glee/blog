9521cfa0e8bcf1ae2d68f08f08006d99a14cb980
Takuma Yoshimi <ysm0706glee@gmail.com>
2023-10-14 10:25:24 +0200

    update blog composable

--- composables/useBlog.ts
+++ composables/useBlog.ts
@@ -1,18 +1,29 @@
-import { Blog } from "~/types/blog";
 import { Database } from "~/types/supabase";
+import { Blog } from "~/types/blog";
+import { Tag } from "~/types/tag";
 
-const useBlog = () => {
+export const useBlog = () => {
   const supabase = useSupabaseClient<Database>();
 
   const blogsState = ref<Blog[]>([]);
   const offset = ref(0);
-  const limit = 10;
+  const limit = ref(10);
+  const isFetching = ref(false);
+  const hasMoreData = ref(true);
+
+  const setIsFetching = (value: boolean) => {
+    isFetching.value = value;
+  };
+
+  const setHasMoreData = (value: boolean) => {
+    hasMoreData.value = value;
+  };
 
   const getBlogs = async (
     offset: number,
     limit: number
   ): Promise<Blog[] | null> => {
-    const { data: blogs, error } = await supabase
+    const { data, error } = await supabase
       .from("blogs")
       .select("*")
       .range(offset, offset + limit - 1);
@@ -20,37 +31,114 @@ const useBlog = () => {
       console.error(error);
       return null;
     }
-    blogsState.value.push(...blogs);
-    return blogs;
+    blogsState.value.push(...data);
+    return data;
   };
 
-  const postBlog = async (
-    blog: Pick<Blog, "url" | "title" | "description" | "image">
-  ): Promise<Blog | null> => {
-    const { data: newBlog, error } = await supabase
+  const getBlogsWithInfiniteScroll = async () => {
+    if (!hasMoreData.value) return;
+    setIsFetching(true);
+    await new Promise((resolve) => setTimeout(resolve, 2000));
+    const blogs = await getBlogs(offset.value, limit.value);
+    if (blogs?.length) {
+      offset.value += limit.value;
+    } else {
+      setHasMoreData(false);
+    }
+    setIsFetching(false);
+  };
+
+  const getBlogsByTags = async (
+    tags: Tag[],
+    offset: number,
+    limit: number
+  ): Promise<Blog[] | null> => {
+    const { data, error } = await supabase
       .from("blogs")
-      .insert({
-        url: blog.url,
-        title: blog.title,
-        description: blog.description,
-        image: blog.image,
-      })
-      .select();
+      .select(` *, tags!inner (*)`)
+      .in(
+        "tags.id",
+        tags.map((tag) => tag.id)
+      )
+      .range(offset, offset + limit - 1);
+    console.log(data);
     if (error) {
       console.error(error);
       return null;
     }
-    blogsState.value = [newBlog[0], ...blogsState.value];
-    return newBlog[0];
+    blogsState.value.push(...data);
+    return data;
+  };
+
+  const getBlogsWithInfiniteScrollByTags = async (tags: Tag[]) => {
+    if (!hasMoreData.value) return;
+    setIsFetching(true);
+    await new Promise((resolve) => setTimeout(resolve, 2000));
+    const blogs = await getBlogsByTags(tags, offset.value, limit.value);
+    if (blogs?.length) {
+      offset.value += limit.value;
+    } else {
+      setHasMoreData(false);
+    }
+    setIsFetching(false);
+  };
+
+  const postBlogTags = async (
+    blogId: number,
+    tags: Tag[]
+  ): Promise<boolean> => {
+    const { error } = await supabase.from("blog_tags").insert(
+      tags.map((tag) => ({
+        blog_id: blogId,
+        tag_id: tag.id,
+      }))
+    );
+    if (error) {
+      console.error("Error posting blog tags:", error);
+      return false;
+    }
+    return true;
+  };
+
+  const postBlog = async (
+    blog: Pick<Blog, "url" | "title" | "description" | "image">,
+    tags?: Tag[]
+  ): Promise<Blog | null> => {
+    const { data, error } = await supabase.from("blogs").insert(blog).select();
+    if (error) {
+      console.error("Error posting blog:", error);
+      return null;
+    }
+    const newBlog = data[0];
+    if (tags?.length) {
+      const success = await postBlogTags(newBlog.id, tags);
+      if (!success) {
+        console.error(
+          "Failed to post blog tags. Consider rolling back blog insertion."
+        );
+        return null;
+      }
+    }
+    blogsState.value = [newBlog, ...blogsState.value];
+    return newBlog;
   };
 
   return {
     blogsState,
     offset,
     limit,
+    isFetching,
+    hasMoreData,
+    setIsFetching,
+    setHasMoreData,
     getBlogs,
+    getBlogsWithInfiniteScroll,
+    getBlogsByTags,
+    getBlogsWithInfiniteScrollByTags,
     postBlog,
   };
 };
 
-export default useBlog;
+export type UseBlog = ReturnType<typeof useBlog>;
+
+export const blogInjectionKey: InjectionKey<UseBlog> = Symbol("use-blog");
