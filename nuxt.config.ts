1bb7087895e0d8c98cb33e2dcd240ef85fb0d689
Takuma Yoshimi <ysm0706glee@gmail.com>
2023-10-19 17:46:42 +0200

    fix supabaseUrl is required. error

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,6 +4,8 @@ export default defineNuxtConfig({
   modules: ["@nuxtjs/supabase", "@nuxt/ui"],
   supabase: {
     redirect: false,
+    url: process.env.SUPABASE_URL,
+    key: process.env.SUPABASE_KEY,
   },
   build: {
     transpile: ["trpc-nuxt"],
