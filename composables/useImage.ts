68a33a055b4bcb0cdcd84f32ac7a09c098e85834
Takuma Yoshimi <ysm0706glee@gmail.com>
2023-11-17 15:36:07 +0100

    update useImage

--- composables/useImage.ts
+++ composables/useImage.ts
@@ -1,53 +1,19 @@
-import {
-  S3Client,
-  PutObjectCommand,
-  DeleteObjectCommand,
-} from "@aws-sdk/client-s3";
-
-const BUCKET_NAME_DEVELOP = "blog-develop";
-
-const BUCKET_NAME_PRODUCTION = "blog-production";
-
 export const useImage = () => {
-  const runtimeConfig = useRuntimeConfig();
-
-  const BUCKET_NAME =
-    runtimeConfig.public.ENVIRONMENT === "production"
-      ? BUCKET_NAME_PRODUCTION
-      : BUCKET_NAME_DEVELOP;
-
-  const S3 = new S3Client({
-    region: "auto",
-    endpoint: `https://${runtimeConfig.public.CLOUDFLARE_API_SECRET}.r2.cloudflarestorage.com`,
-    credentials: {
-      accessKeyId: runtimeConfig.public.CLOUDFLARE_ACCESS_KEY_ID,
-      secretAccessKey: runtimeConfig.public.CLOUDFLARE_SECRET_ACCESS_KEY,
-    },
-  });
-
   const postImage = async (file: File) => {
-    const key = `${Date.now()}-${file.name}`;
-    await S3.send(
-      new PutObjectCommand({
-        Bucket: BUCKET_NAME,
-        Key: key,
-        Body: file,
-      })
-    );
-    const url =
-      runtimeConfig.public.ENVIRONMENT === "production"
-        ? `https://takumaaa.dev/${key}`
-        : `https://pub-cee5b3218211426eb6dfde5132bc4685.r2.dev/${key}`;
-    return { url, key };
+    const formData = new FormData();
+    formData.append("file", file);
+    const response = await useFetch("/api/image", {
+      method: "put",
+      body: formData,
+    });
+    return response.data.value;
   };
 
   const deleteImage = async (key: string) => {
-    await S3.send(
-      new DeleteObjectCommand({
-        Bucket: BUCKET_NAME,
-        Key: key,
-      })
-    );
+    const response = await useFetch(`/api/image?key=${key}`, {
+      method: "delete",
+    });
+    return response.data.value;
   };
 
   return {
