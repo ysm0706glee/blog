4e622a72616c0b4fdff3bd189ec4a679cbd1cdd2
Takuma Yoshimi <ysm0706glee@gmail.com>
2023-11-19 10:14:50 +0100

    test

--- server/api/image/index.put.ts
+++ server/api/image/index.put.ts
@@ -26,7 +26,9 @@ export default defineEventHandler(async (event) => {
     }
   );
   if (!response.ok) {
-    throw new Error(`HTTP error! status: ${response.status}`);
+    throw new Error(
+      `HTTP error! status: ${response.status}` + process.env.NODE_ENV
+    );
   }
   try {
     const data = await response.json();
