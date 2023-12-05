5b02877c3857b7767f22ee2e7aeec5d2d3d00417
Takuma Yoshimi <ysm0706glee@gmail.com>
2023-12-05 15:39:46 +0100

    remove supabase

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -12,12 +12,7 @@ const d1ApiUrl =
 
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  modules: ["@nuxtjs/supabase", "@nuxt/ui"],
-  supabase: {
-    redirect: false,
-    url: process.env.SUPABASE_URL,
-    key: process.env.SUPABASE_KEY,
-  },
+  modules: ["@nuxt/ui"],
   ui: {
     icons: ["bxl"],
   },
